82cbccc44592d566152fe9c47c05f3777d477a64
PeihanK <konstantinpeihan@gmail.com>
2024-07-10 22:52:09 +0200

    lesson2_practicum

--- main.py
+++ main.py
@@ -13,5 +13,20 @@ def user(name):
     return f'Welcome, {name}!'
 
 
+@app.route('/double/<int:number>')
+def double_number(number):
+    return f'{number} doubled is {number * 2}'
+
+
+@app.route('/square/<float:number>')
+def square_number(number):
+    return f'The square of {number} is {number * number}'
+
+
+@app.route('/reverse/<path:text>')
+def reverse_text(text):
+    return f'{text} reversed is {text[::-1]}'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
